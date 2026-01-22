f6535deaea5f498228e1c96753cfad45671f014e
Marqui <bmay86@protonmail.com>
2026-01-21 23:40:48 -0500

    Updated Logo height

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import './Logo.css';
 const Logo = () => {
   return (
     <Tilt>
-      <div className='ma4 mt0' style={{ height: '300px' }}>
+      <div className='ma4 mt0' style={{ height: '150px' }}>
         <h1>React Parallax Tilt 👀</h1>
       </div>
     </Tilt>
