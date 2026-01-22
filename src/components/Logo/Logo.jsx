55606634afa98b9bbb417693945a108730ef156b
Marqui <bmay86@protonmail.com>
2026-01-21 23:55:31 -0500

    Imported brain.png and created img tag for the logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,12 @@
 import Tilt from 'react-parallax-tilt'
+import brain from './brain.png'
 import './Logo.css'
 
 const Logo = () => {
   return (
     <Tilt className="tilt">
       <div className="ma4 mt0" style={{ height: 250 }}>
-        <h1>React Parallax Tilt 👀</h1>
+        <h1>React Parallax Tilt <img src={brain} alt="brain" /></h1>
       </div>
     </Tilt>
   )
