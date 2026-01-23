d31376625853b47448ab77995a2f423c26bbb592
Marqui <bmay86@protonmail.com>
2026-01-23 03:39:13 -0500

    Clean up code

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,19 +1,21 @@
-import Tilt from 'react-parallax-tilt'
-import brain from './brain.png'
-import './Logo.css'
+import Tilt from "react-parallax-tilt";
+import brain from "./brain.png";
+import "./Logo.css";
 
 const Logo = () => {
   return (
     <Tilt className="tilt">
-      <div className="ma4 mt0 tilt-inner pa3" style={{ paddingTop: '5px', height: 250 }}>
-        <h1 style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+      <div
+        className="ma4 mt0 tilt-inner pa3"
+        style={{ paddingTop: "5px", height: 250 }}
+      >
+        <h1 style={{ display: "flex", alignItems: "center", gap: "8px" }}>
           React Parallax Tilt
           <img src={brain} alt="Brain logo" style={{ height: 40 }} />
         </h1>
       </div>
     </Tilt>
-  )
-}
+  );
+};
 
-export default Logo
-
+export default Logo;
