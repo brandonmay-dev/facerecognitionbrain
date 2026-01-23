e711c1f6f4dfa5b740e9c26bdeb58cba176b3aa0
Marqui <bmay86@protonmail.com>
2026-01-23 02:41:44 -0500

    Logo styling, tilt-inner padding and paddingTop

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import './Logo.css'
 const Logo = () => {
   return (
     <Tilt className="tilt">
-      <div className="ma4 mt0" style={{ height: 250 }}>
+      <div className="ma4 mt0 tilt-inner pd3" style={{ paddingTop: '5px', height: 250 }}>
         <h1>React Parallax Tilt <img src={brain} alt="brain" /></h1>
       </div>
     </Tilt>
