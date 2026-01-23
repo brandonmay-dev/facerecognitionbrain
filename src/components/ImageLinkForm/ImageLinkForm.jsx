3bad3e9ee023485e4c63c1505949721adf65a8af
Marqui <bmay86@protonmail.com>
2026-01-23 03:05:59 -0500

    Cleaned up code

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,16 +1,18 @@
-
-
 const ImageLinkForm = () => {
   return (
-      <div>
-        <p className='f3'> {'This Magic Brain will detect faces in your pictures. Give it a try.'}
-        </p>
-      </div>
-      <div>
-        <input className='f4 pa2 w-70 center' type='text' />
-        <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
-      </div>
-  );
-}
+    <div>
+      <p className="f3">
+        This Magic Brain will detect faces in your pictures. Give it a try.
+      </p>
 
-export default ImageLinkForm
+      <div>
+        <input className="f4 pa2 w-70 center" type="text" />
+        <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+          Detect
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ImageLinkForm;
