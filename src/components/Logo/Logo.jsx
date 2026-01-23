f92269357789ac8cc4e6de221c09650f9c7cfd7d
Marqui <bmay86@protonmail.com>
2026-01-23 02:45:39 -0500

    Enhance Logo component with react-parallax-tilt and improved header layout

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,11 +5,15 @@ import './Logo.css'
 const Logo = () => {
   return (
     <Tilt className="tilt">
-      <div className="ma4 mt0 tilt-inner pd3" style={{ paddingTop: '5px', height: 250 }}>
-        <h1>React Parallax Tilt <img src={brain} alt="brain" /></h1>
+      <div className="ma4 mt0 tilt-inner pa3" style={{ paddingTop: '5px', height: 250 }}>
+        <h1 style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+          React Parallax Tilt
+          <img src={brain} alt="Brain logo" style={{ height: 40 }} />
+        </h1>
       </div>
     </Tilt>
   )
 }
 
 export default Logo
+
