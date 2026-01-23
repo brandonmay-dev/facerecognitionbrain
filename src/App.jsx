0d36df99b363c6acd7f9a487f5f05ffc7b27a9f2
Marqui <bmay86@protonmail.com>
2026-01-23 17:09:29 -0500

    Add ParticlesBg import and code

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Navigation from "./components/Navigation/Navigation.jsx";
 import Logo from "./components/Logo/Logo.jsx";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.jsx";
 import Rank from "./components/Rank/Rank.jsx";
+import ParticlesBg from "particles-bg";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <ParticlesBg type="circle" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
