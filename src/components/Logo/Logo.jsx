b5a1e8ce63b33c4968eec7f78588d383a4e55388
Marqui <bmay86@protonmail.com>
2026-01-24 12:18:18 -0500

    Move Logo layout styles to CSS

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,13 +5,10 @@ import "./Logo.css";
 const Logo = () => {
   return (
     <Tilt className="tilt">
-      <div
-        className="ma4 mt0 tilt-inner pa3"
-        style={{ paddingTop: "5px", height: 250 }}
-      >
-        <h1 style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+      <div className="ma4 mt0 tilt-inner pa3 logo-container">
+        <h1 className="logo-title">
           React Parallax Tilt
-          <img src={brain} alt="Brain logo" style={{ height: 40 }} />
+          <img src={brain} alt="Brain logo" className="logo-image" />
         </h1>
       </div>
     </Tilt>
