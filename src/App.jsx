e1f3af408f912e34cf4a57035513660231a1919d
Marqui <bmay86@protonmail.com>
2026-01-24 12:06:58 -0500

    Change type

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <ParticlesBg type="circle" bg={true} />
+      <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
