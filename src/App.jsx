d84cefc081c6bbd2438cfab00de4f419a89502a4
Marqui <bmay86@protonmail.com>
2026-01-25 03:09:43 -0500

    Add input state handling and pass input change handler to ImageLinkForm

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,24 @@ import ParticlesBg from "particles-bg";
 import "./App.css";
 
 function App() {
+  constructor(
+    super();
+    this.state = {
+      input: "", 
+    };
+  }
+
+onInputChange = (event) => {
+    console.log(event);
+  }
+
   return (
     <div>
       <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={this.onInputChange} />
       {/* <FaceRecognition /> */}
     </div>
   );
