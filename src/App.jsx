2aa3eabdbbd871c9985b9e64a8c67ee17cade4b1
Marqui <bmay86@protonmail.com>
2026-01-26 18:41:44 -0500

    Update clarifai code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { Component } from "react";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition.jsx";
 import Navigation from "./components/Navigation/Navigation.jsx";
 import Logo from "./components/Logo/Logo.jsx";
@@ -7,53 +7,77 @@ import Rank from "./components/Rank/Rank.jsx";
 import ParticlesBg from "particles-bg";
 import "./App.css";
 
-function App() {
-  const [input, setInput] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const [clarifaiBox, setClarifaiBox] = useState(null);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      clarifaiBox: null,
+    };
+  }
 
-  const onInputChange = (event) => {
-    setInput(event.target.value);
+  onInputChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   };
 
-  const onButtonsubmit = () => {
-    setImageUrl(input);
-    setClarifaiBox(null);
+  onButtonsubmit = () => {
+    const PAT = "Y5e48176b9fd14372bcc5db6dae1b5723";
+    const USER_ID = "clarifai";
+    const APP_ID = "main";
+    const MODEL_ID = "face-detection";
 
-    fetch("https://api.clarifai.com/v2/models/${MODEL_ID}/outputs", {
+    this.setState({ imageUrl: this.state.input });
+
+    fetch(`https://api.clarifai.com/v2/models/${MODEL_ID}/outputs`, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ imageUrl: input }),
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_app_id: {
+          user_id: USER_ID,
+          app_id: APP_ID,
+        },
+        inputs: [
+          {
+            data: {
+              image: {
+                url: this.state.input,
+              },
+            },
+          },
+        ],
+      }),
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log("Clarifai response (via proxy):", data);
-
-        const region = data?.outputs?.[0]?.data?.regions?.[0];
-        if (!region) {
-          console.log("No face detected");
-          setClarifaiBox(null);
-          return;
-        }
-
-        setClarifaiBox(region.region_info.bounding_box); // ratios (0..1)
+        console.log(data);
+        // next step: calculate face box + setState
       })
-      .catch((err) => console.log("Proxy error:", err));
+      .catch(console.log);
   };
 
-  return (
-    <div>
-      <ParticlesBg type="cobweb" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm
-        onInputChange={onInputChange}
-        onButtonsubmit={onButtonsubmit}
-      />
-      <FaceRecognition imageUrl={imageUrl} clarifaiBox={clarifaiBox} />
-    </div>
-  );
+  render() {
+    const { imageUrl, clarifaiBox } = this.state;
+
+    return (
+      <div>
+        <ParticlesBg type="cobweb" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonsubmit={this.onButtonsubmit}
+        />
+        <FaceRecognition imageUrl={imageUrl} clarifaiBox={clarifaiBox} />
+      </div>
+    );
+  }
 }
 
 export default App;
