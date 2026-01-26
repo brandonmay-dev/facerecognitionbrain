52526b4acf6ae47eb54fd604e31f8c72cc8458db
Marqui <bmay86@protonmail.com>
2026-01-26 18:49:17 -0500

    Update and clean code

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,49 +1,30 @@
-import { useState } from "react";
 import "./FaceRecognition.css";
 
 const FaceRecognition = ({ imageUrl, clarifaiBox }) => {
-  const [box, setBox] = useState(null);
-
-  const onImageLoad = (e) => {
-    if (!clarifaiBox) {
-      setBox(null);
-      return;
-    }
-
-    const width = e.target.width;
-    const height = e.target.height;
-
-    setBox({
-      leftCol: clarifaiBox.left_col * width,
-      topRow: clarifaiBox.top_row * height,
-      rightCol: width - clarifaiBox.right_col * width,
-      bottomRow: height - clarifaiBox.bottom_row * height,
-    });
-  };
-
   return (
     <div className="center">
       <div
-        style={{ position: "relative", display: "inline-block" }}
         className="mt2"
+        style={{ position: "relative", display: "inline-block" }}
       >
         {imageUrl && (
           <>
             <img
+              id="inputimage"
               alt=""
               src={imageUrl}
               width="500px"
               height="auto"
-              onLoad={onImageLoad}
             />
-            {box && (
+
+            {clarifaiBox && (
               <div
                 className="bounding-box"
                 style={{
-                  top: box.topRow,
-                  right: box.rightCol,
-                  bottom: box.bottomRow,
-                  left: box.leftCol,
+                  top: clarifaiBox.topRow,
+                  right: clarifaiBox.rightCol,
+                  bottom: clarifaiBox.bottomRow,
+                  left: clarifaiBox.leftCol,
                 }}
               />
             )}
