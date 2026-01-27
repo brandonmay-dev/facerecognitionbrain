ce9e3a2e29e057efee79cbe2398dc9f3ad297c2a
Marqui <bmay86@protonmail.com>
2026-01-27 18:37:25 -0500

    Style article className

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,6 +1,6 @@
 const Signin = () => {
   return (
-    <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
+    <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
         <form className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
