e3cf86e4a3257bb39da5fc3dc2dfe352e04ac042
Marqui <bmay86@protonmail.com>
2026-01-27 20:40:35 -0500

    Add pointer to Register

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -49,7 +49,7 @@ const Signin = ({ onRouteChange }) => {
           <div className="lh-copy mt3">
             <p
               onClick={() => onRouteChange("register")}
-              className="f6 link dim black db"
+              className="f6 link dim black db pointer"
             >
               Register
             </p>
