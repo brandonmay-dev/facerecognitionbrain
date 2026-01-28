954e6c96c655930d869603f7e6b0a8d6e7442582
Marqui <bmay86@protonmail.com>
2026-01-27 20:57:32 -0500

    Update signin and register nav routing

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,29 @@
-const Navigation = ({ onRouteChange }) => {
+const Navigation = ({ onRouteChange, isSignedIn }) => {
   return (
     <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-      <p
-        onClick={() => onRouteChange("signin")}
-        className="f3 link dim black underline pa3 pointer"
-      >
-        Sign Out
-      </p>
+      {isSignedIn ? (
+        <p
+          onClick={() => onRouteChange("signin")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign Out
+        </p>
+      ) : (
+        <>
+          <p
+            onClick={() => onRouteChange("signin")}
+            className="f3 link dim black underline pa3 pointer"
+          >
+            Sign In
+          </p>
+          <p
+            onClick={() => onRouteChange("register")}
+            className="f3 link dim black underline pa3 pointer"
+          >
+            Register
+          </p>
+        </>
+      )}
     </nav>
   );
 };
