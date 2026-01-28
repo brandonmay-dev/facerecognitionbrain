bbd70a98a6ef37b83e512b18c5fcbd9b9d6cdcaa
Marqui <bmay86@protonmail.com>
2026-01-27 19:58:48 -0500

    Change Sign In header to Register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -10,7 +10,7 @@ const Register = ({ onRouteChange }) => {
           }}
         >
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+            <legend className="f1 fw6 ph0 mh0">Register</legend>
 
             <div className="mt3">
               <label className="db fw6 lh-copy f6" htmlFor="name">
