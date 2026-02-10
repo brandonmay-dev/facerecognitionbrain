c1e316025a022cb290466d94bb2d0d5157e5006b
Marqui <bmay86@protonmail.com>
2026-02-10 15:50:16 -0500

    Wire Register component to backend register API

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,57 +1,75 @@
+import { useState } from "react";
+
 const Register = ({ onRouteChange }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitRegister = () => {
+    fetch("http://localhost:3001/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("register failed");
+        }
+        return res.json();
+      })
+      .then((user) => {
+        if (user && user.id) {
+          onRouteChange("home");
+        }
+      })
+      .catch((err) => {
+        console.log("Register error:", err);
+        alert("Unable to register");
+      });
+  };
+
   return (
-    <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+    <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
-        <div
-          className="measure"
-          onSubmit={(e) => {
-            e.preventDefault();
-            onRouteChange("home");
-          }}
-        >
-          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+        <div className="measure">
+          <fieldset id="register" className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Register</legend>
 
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="name">
-                Name
-              </label>
+              <label className="db fw6 lh-copy f6">Name</label>
               <input
-                className="pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="text"
-                name="name"
-                id="name"
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
 
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                Email
-              </label>
+              <label className="db fw6 lh-copy f6">Email</label>
               <input
-                className="pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="email"
-                name="email-address"
-                id="email-address"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
 
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="password">
-                Password
-              </label>
+              <label className="db fw6 lh-copy f6">Password</label>
               <input
-                className="b pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="password"
-                name="password"
-                id="password"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </fieldset>
 
           <div>
             <input
-              onClick={() => onRouteChange("home")}
+              onClick={onSubmitRegister}
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
               value="Register"
