490c289914fc0aebf109cf3f26a90d431d0619a0
Marqui <bmay86@protonmail.com>
2026-04-02 22:50:11 -0400

    feat(forms): enable submit on Enter using form onSubmit handler

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -36,7 +36,13 @@ const Register = ({ loadUser, onRouteChange }) => {
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
-        <div className="measure">
+        <form
+          className="measure"
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSubmitRegister();
+          }}
+        >
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Register</legend>
 
@@ -47,44 +53,34 @@ const Register = ({ loadUser, onRouteChange }) => {
               <input
                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="text"
-                id="name"
                 onChange={(e) => setName(e.target.value)}
               />
             </div>
 
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                Email
-              </label>
+              <label className="db fw6 lh-copy f6">Email</label>
               <input
                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="email"
-                id="email-address"
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
 
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="password">
-                Password
-              </label>
+              <label className="db fw6 lh-copy f6">Password</label>
               <input
                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="password"
-                id="password"
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </fieldset>
 
-          <div>
-            <input
-              onClick={onSubmitRegister}
-              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-              type="submit"
-              value="Register"
-            />
-          </div>
+          <input
+            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+            type="submit"
+            value="Register"
+          />
 
           <div className="lh-copy mt3">
             <p
@@ -94,7 +90,7 @@ const Register = ({ loadUser, onRouteChange }) => {
               Sign in
             </p>
           </div>
-        </div>
+        </form>
       </main>
     </article>
   );
--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -11,14 +11,11 @@ const Signin = ({ onRouteChange, loadUser }) => {
       body: JSON.stringify({ email, password }),
     })
       .then(async (res) => {
-        const text = await res.text(); // read body no matter what
+        const text = await res.text();
         console.log("signin status:", res.status);
         console.log("signin response:", text);
 
-        if (!res.ok) {
-          // show the real backend message
-          throw new Error(text);
-        }
+        if (!res.ok) throw new Error(text);
 
         return JSON.parse(text);
       })
@@ -39,43 +36,40 @@ const Signin = ({ onRouteChange, loadUser }) => {
   return (
     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
-        <div className="measure">
-          <fieldset id="sign_in" className="ba b--transparent ph0 mh0">
+        <form
+          className="measure"
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSubmitSignIn();
+          }}
+        >
+          <fieldset className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
 
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                Email
-              </label>
+              <label className="db fw6 lh-copy f6">Email</label>
               <input
                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="email"
-                id="email-address"
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
 
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="password">
-                Password
-              </label>
+              <label className="db fw6 lh-copy f6">Password</label>
               <input
                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="password"
-                id="password"
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </fieldset>
 
-          <div>
-            <input
-              onClick={onSubmitSignIn}
-              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-              type="submit"
-              value="Sign in"
-            />
-          </div>
+          <input
+            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+            type="submit"
+            value="Sign in"
+          />
 
           <div className="lh-copy mt3">
             <p
@@ -85,7 +79,7 @@ const Signin = ({ onRouteChange, loadUser }) => {
               Register
             </p>
           </div>
-        </div>
+        </form>
       </main>
     </article>
   );
