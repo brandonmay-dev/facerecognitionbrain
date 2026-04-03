47b225b88af574e024c405a85491593af7c753b7
Marqui <bmay86@protonmail.com>
2026-04-03 00:58:51 -0400

    fix: allow Heroku host in Vite preview

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: true,
+    allowedHosts: ["smart-brain-app-32fac457676f.herokuapp.com"],
+  },
+});
